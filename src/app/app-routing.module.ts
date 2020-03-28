dd8be97e86be0567ff6eaba1c25608b1b3e53d73
casparsylwan <casparsylwan@gmail.com>
2020-03-28 12:30:26 +0100

    firstcomit with frontpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomefrontComponent } from './components/homefront/homefront.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomefrontComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { HomefrontComponent } from './components/homefront/homefront.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    HomefrontComponent
   ],
   imports: [
     BrowserModule,
